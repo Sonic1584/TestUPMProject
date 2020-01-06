02fb9a43acc9f3016286465bad43cbe1d00aaba4
Sonic1584 <59092870+Sonic1584@users.noreply.github.com>
2020-01-06 11:06:31 +0100

    Add files via upload

---
+++ Project_Package/Editor/TestEditor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEditor;
+
+    public class TestEditor
+    {
+        [MenuItem("TEST_1/Do something cool")]
+        public static void DoSomething(MenuCommand command)
+        {
+
+        }
+
+        [MenuItem("TEST_2/Do something cool again")]
+        public static void DoSomethingAgain(MenuCommand command)
+        {
+
+        }
+}
+
